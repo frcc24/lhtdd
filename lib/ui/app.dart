ddd7668f3715eaca01a406148ace81a014b2e3ee
frcc24 <frcc24@gmail.com>
2022-11-02 18:22:08 -0300

    add login page icon

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -4,10 +4,24 @@ import 'package:untitled1/ui/login/login_page.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Color.fromRGBO(208, 41, 49, 1.0);
+    final primaryColorDark = Color.fromRGBO(138, 28, 33, 1.0);
+    final primaryColorLight = Color.fromRGBO(217, 79, 84, 1.0);
+    final secondaryColor = Color.fromRGBO(211, 47, 47, 1.0);
+
     return MaterialApp(
       title: 'LHTDD',
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      theme: ThemeData(
+        primaryColor: secondaryColor,
+        primaryColorDark: primaryColorDark,
+        primaryColorLight: primaryColorLight,
+        backgroundColor: Colors.white,
+        accentColor: secondaryColor,
+        // colorScheme:
+        //     ColorScheme.fromSwatch().copyWith(secondary: secondaryColor)
+      ),
+      home: const LoginPage(),
     );
   }
 }
--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'local_widgets/local_wodgets.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -7,45 +9,43 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Container(
-          color: Colors.white,
-          child: Column(
-            children: [
-              Container(
-                child: const Image(
-                  image: AssetImage('assets/app_icon.png'),
-                ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const LoginHeader(),
+            Text(
+              'Login'.toUpperCase(),
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      icon: Icon(Icons.email),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      icon: Icon(Icons.lock),
+                    ),
+                    obscureText: true,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {}, child: Text('LogIn'.toUpperCase())),
+                  TextButton.icon(
+                    onPressed: () {},
+                    label: const Text('Create Account'),
+                    icon: const Icon(Icons.person),
+                  ),
+                ],
               ),
-              Text('Login'.toUpperCase()),
-              Form(
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        icon: Icon(Icons.email),
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        icon: Icon(Icons.lock),
-                      ),
-                      obscureText: true,
-                    ),
-                    ElevatedButton(
-                        onPressed: () {}, child: Text('LogIn'.toUpperCase())),
-                    TextButton.icon(
-                      onPressed: () {},
-                      label: Text('Create Account'),
-                      icon: Icon(Icons.person),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
